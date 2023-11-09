ce8ae5e5a46f336da8cf4d48f283a2a4c475cf12
Adeilla <azahrailla563@gmail.com>
2023-11-09 11:05:00 +0700

    ok

---
+++ src/skrip-utama.py
@@ -0,0 +1,8 @@
+from luas import lingkaran, persegi, segitiga
+print(lingkaran.luas_lingkaran(12))
+print(persegi.luas_persegi(21))
+print(segitiga.luas_segitiga(20,2))
+from volume import bola, kubus, tabung
+print(bola.volume_bola(40))
+print(kubus.volume_kubus(32))
+print(tabung.volume_tabung(42,4))
---
+++ src/volume/tabung.py
@@ -0,0 +1,2 @@
+def volume_tabung(r, tinggi):
+  return 22/7 * r * r * tinggi
